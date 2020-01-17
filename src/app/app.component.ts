ab6452c526cb4c746ba5eabd17b1918c6656fb88
Maria <Curso@IPARTEKAULA.COM>
2020-01-17 09:43:27 +0100

    app-angular inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+// componente principal  que se carga al arrancar la app
+
+
+/*
+  @Component es la forma de declarar un componente (se llama decorador)
+    selector :    nombre de la etiqueta para inyectar el componente en html
+    templateUrl:  vista de HTML
+    styleUrls:    estilos de la vista formato scss 
+  */
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// title es una variable
 export class AppComponent {
   title = 'my-app';
 }
