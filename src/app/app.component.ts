3302ce42037793b595a6c681ae2b95b9626261e9
Maria <Curso@IPARTEKAULA.COM>
2020-01-17 12:42:08 +0100

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 // componente principal  que se carga al arrancar la app
 
 
@@ -17,5 +18,91 @@ import { Component } from '@angular/core';
 
 // title es una variable
 export class AppComponent {
+
+
+
   title = 'my-app';
+  productos =  [
+    {
+    "id": 34,
+    "nombre": "cafe",
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG",
+    "gluten": false,
+    "precio": 1.46,
+    "categoria": {
+      "id": 1,
+      "nombre": "bebida"
+    },
+    "historico": [{
+        "fecha": "17/01/2020",
+        "precio": 1.20
+      },
+      {
+        "fecha": "11/01/2020",
+        "precio": 1.50
+      },
+      {
+        "fecha": "12/01/2020",
+        "precio": 1.46
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "nombre": "leche",
+    "imagen": "https://supermercado.eroski.es/images/18672311.jpg",
+    "gluten": false,
+    "precio": 2.46,
+    "categoria": {
+      "id": 1,
+      "nombre": "bebida"
+    },
+    "historico": [{
+        "fecha": "17/01/2020",
+        "precio": 2.20
+      },
+      {
+        "fecha": "11/01/2020",
+        "precio": 2.50
+      },
+      {
+        "fecha": "12/01/2020",
+        "precio": 2.46
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "nombre": "galletas",
+    "imagen": "https://supermercado.eroski.es/images/16863292.jpg",
+    "gluten": true,
+    "precio": 4.66,
+    "categoria": {
+      "id": 2,
+      "nombre": "dulces"
+    },
+    "historico": [{
+        "fecha": "17/01/2020",
+        "precio": 5
+      },
+      {
+        "fecha": "11/01/2020",
+        "precio": 4.6
+      },
+      {
+        "fecha": "12/01/2020",
+        "precio": 4.66
+      }
+    ]
+  }
+];
+
+pSeleccionado = this.productos[0];
+
+seleccionarProducto = function(producto){
+  console.log("hemos hecho click");
+  console.debug('%o' ,producto);
+  this.pSeleccionado = producto;
 }
+
+};
