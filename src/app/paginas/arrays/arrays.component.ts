f1a626563c3493ee130dd74fbf657d2ebfff8cb5
Maria <Curso@IPARTEKAULA.COM>
2020-01-27 14:00:10 +0100

    arrays

--- src/app/paginas/arrays/arrays.component.ts
+++ src/app/paginas/arrays/arrays.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { findReadVarNames } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-arrays',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArraysComponent implements OnInit {
 
-  constructor() { }
+
+  frutas: any;
+  total: number;
+
+  constructor() {
+    console.trace('ArraysComponent constructor')
+    this.frutas = [
+      {"nombre":"fresa", "precio":2.45},
+      {"nombre":"pera", "precio":2.50},
+      {"nombre":"manzana", "precio":1.99}
+    ];
+ 
+    this.total = this.frutas.map(function(x){
+      return this.total+=x.precio;
+    });
+  }
 
   ngOnInit() {
+    console.trace('ArraysComponent ngOnInit')
   }
 
 }
