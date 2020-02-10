78f0d3b0bc706322af69bff42eea0f845553d6cf
Maria <Curso@IPARTEKAULA.COM>
2020-02-10 13:50:49 +0100

    diseño

--- src/app/paginas/tareas/tareas.component.ts
+++ src/app/paginas/tareas/tareas.component.ts
@@ -10,11 +10,15 @@ import { TareasService } from 'src/app/services/tareas.service';
 export class TareasComponent implements OnInit {
 
   tareas: Array<Tarea>;
-  tarea: Tarea;
+  tituloNuevo: string;
+  mensaje:string;
+
 
   constructor(private servicioTarea: TareasService) {
     console.trace('Constructor TareasComponent');
     this.tareas = []; //inicializar
+    this.tituloNuevo = '';
+    this.mensaje = '';
 
 
   }//constructor
@@ -32,6 +36,32 @@ export class TareasComponent implements OnInit {
   }//ngOnInit
 
 
+  validarTitulo(titulo:String): boolean{
+    return (titulo.trim().length <=1) ? false : true;
+  }
+
+  nuevaTarea():void{
+    console.debug('click nueva tarea %s', this.tituloNuevo);
+    if (this.validarTitulo(this.tituloNuevo)==false){this.mensaje = 'No puede crear una tarea vacía ni de una letra';}
+    else{
+
+      //crear objeto tarea
+      let tareaNueva = new Tarea();
+      tareaNueva.titulo = this.tituloNuevo;
+      console.debug('tareaNueva', tareaNueva);
+  
+      //llamar servicio
+      this.servicioTarea.crear(tareaNueva).subscribe( (dato) => {
+        console.debug('nueva tarea %o',dato);
+        this.cargarTareas();
+        this.mensaje = `Creada con éxito la tarea  ${dato.id}  ${dato.titulo}`;
+      });
+    
+      this.tituloNuevo = '';
+    }
+
+  }
+
   editarEstado(tarea: Tarea) {
     console.debug('click %o', tarea);
     tarea.completada = !tarea.completada;
@@ -41,6 +71,16 @@ export class TareasComponent implements OnInit {
     });
   }//editarEstado
 
+  editarTitulo(tarea:Tarea){
+    console.debug('click %o', tarea);
+    if ( this.validarTitulo(tarea.titulo)==false) {this.mensaje = 'No puede crear una tarea vacía ni de una letra';}
+    else{
+      this.servicioTarea.modificar(tarea).subscribe( (data) => {
+        this.mensaje = "Tarea modificada con éxito";
+        this.cargarTareas();
+      });
+    }
+  }
 
   eliminar(tarea: Tarea) {
     console.debug('click Eliminar %o', tarea);
@@ -49,6 +89,7 @@ export class TareasComponent implements OnInit {
       console.trace('Confirmado eliminacion');
       this.servicioTarea.eliminar(tarea.id).subscribe(
         () => {
+          this.mensaje = `Eliminada con éxito la tarea ${tarea.id} - ${tarea.titulo}`;
           this.cargarTareas()
         });
 
@@ -68,8 +109,16 @@ export class TareasComponent implements OnInit {
     //llamar al service para obtener tareas
     this.servicioTarea.listar().subscribe((datos) => {
       console.debug('esto se ejecuta de forma asincrona');
-      this.tareas = datos;
-    });
+      this.tareas = datos.reverse();
+      if(this.tareas.length===0){this.mensaje="No hay tareas asignadas";}
+    },
+    (error) => {
+      console.debug('JSON SERVER PARADO',error);
+      this.mensaje = 'Error inesperado: no es posible conectar con el servidor';
+    }
+    
+    
+    );
 
   }//cargarTareas
 
--- src/app/services/tareas.service.ts
+++ src/app/services/tareas.service.ts
@@ -21,27 +21,29 @@ export class TareasService implements ITareasService{
     const url  = "http://localhost:3000/tareas"; 
     console.debug(`GET ${url}`);
     return this.http.get<Array<Tarea>>(url);
-  }
+  }//listar
 
   detalle(id: number): Observable<Tarea> {
     throw new Error("Method not implemented.");
-  }
+  }//detalle
 
   crear(tarea: Tarea): Observable<Tarea> {
-    throw new Error("Method not implemented.");
-  }
+    const url = `http://localhost:3000/tareas/`
+    console.debug('POST %s tarea %o',url, tarea);
+    return this.http.post<Tarea>(url,tarea);
+  }//crear
 
   modificar(tarea: Tarea): Observable<Tarea> {
     const url = `http://localhost:3000/tareas/${tarea.id}`
     console.debug('PUT %s tarea %o',url, tarea);
     return this.http.put<Tarea>(url,tarea);
-  }
+  }//modificar
 
   eliminar(id: number): Observable<Tarea> {
     const url = `http://localhost:3000/tareas/${id}`;
     console.debug('DELETE %s', url);
     return this.http.delete<Tarea>(url);
-  }
+  }//eliminar
   
  
 
